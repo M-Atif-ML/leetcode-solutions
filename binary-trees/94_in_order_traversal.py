25f03904274ef629d614cbc657d64ee9939130c6
MUHAMMAD ATIF KHAN <167567847+M-Atif-ML@users.noreply.github.com>
2025-07-10 23:12:39 +0500

    Create 94_in_order_traversal.py

---
+++ binary-trees/94_in_order_traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            ans.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return ans
+# time complexity is O(n) and space is also O(n) since we are iterating to all nodes and creating a list where number of values depends on number of nodes

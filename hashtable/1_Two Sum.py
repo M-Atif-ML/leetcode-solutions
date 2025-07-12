e7e717d9f0672d1b9fbaf3a287e157271aee9a09
MUHAMMAD ATIF KHAN <167567847+M-Atif-ML@users.noreply.github.com>
2025-07-12 08:29:42 +0500

    Create 1_Two Sum.py

---
+++ hashtable/1_Two Sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+
+    def twoSum(self, nums, target):
+        self.nums = nums
+        self.target = target
+        lenght = len(nums)
+        for i in range(lenght):
+            for j in range(lenght):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i,j]
+          
+        return []
+        
